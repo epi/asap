f57ef1a777f61effe36259ca7ff2ac121caa6a21
Piotr Fusik <fox@scene.pl>
2007-05-02 15:43:28 +0000

    millisecond precision of song length; improved loop detection; don't dump registers of the second POKEY for mono songs

--- asapscan.c
+++ asapscan.c
@@ -48,6 +48,7 @@ static int silence_player_calls;
 static int loop_player_calls;
 static unsigned char *registers_dump;
 
+static const ASAP_ModuleInfo *module_info;
 static int dump = FALSE;
 
 #define FEATURE_CHECK          1
@@ -73,15 +74,16 @@ static int seconds_to_player_calls(int seconds)
 	return (int) (seconds * 1773447.0 / 114.0 / sap_fastplay);
 }
 
-static int player_calls_to_seconds(int player_calls)
+static int player_calls_to_milliseconds(int player_calls)
 {
-	return (int) ceil(player_calls * sap_fastplay * 114.0 / 1773447.0);
+	return (int) ceil(player_calls * sap_fastplay * 114.0 * 1000 / 1773447.0);
 }
 
 void scan_song(int song)
 {
 	int i;
 	int silence_run = 0;
+	int loop_bytes = 18 * loop_player_calls;
 	ASAP_PlaySong(song, 0);
 	for (i = 0; i < scan_player_calls; i++) {
 		unsigned char *p = registers_dump + 18 * i;
@@ -98,11 +100,18 @@ void scan_song(int song)
 		p[17] = AUDCTL[1];
 		if (dump) {
 			printf(
-				"%6.2f: %02X %02X  %02X %02X  %02X %02X  %02X %02X  %02X  |  "
-				"%02X %02X  %02X %02X  %02X %02X  %02X %02X  %02X\n", i * sap_fastplay * 114.0 / 1773447.0,
-				AUDF[0], AUDC[0], AUDF[1], AUDC[1], AUDF[2], AUDC[2], AUDF[3], AUDC[3], AUDCTL[0],
-				AUDF[4], AUDC[4], AUDF[5], AUDC[5], AUDF[6], AUDC[6], AUDF[7], AUDC[7], AUDCTL[1]
+				"%6.2f: %02X %02X  %02X %02X  %02X %02X  %02X %02X  %02X",
+				i * sap_fastplay * 114.0 / 1773447.0,
+				AUDF[0], AUDC[0], AUDF[1], AUDC[1], AUDF[2], AUDC[2], AUDF[3], AUDC[3], AUDCTL[0]
 			);
+			if (module_info->channels > 1) {
+				printf(
+					"  |  %02X %02X  %02X %02X  %02X %02X  %02X %02X  %02X\n",
+					AUDF[4], AUDC[4], AUDF[5], AUDC[5], AUDF[6], AUDC[6], AUDF[7], AUDC[7], AUDCTL[1]
+				);
+			}
+			else
+				printf("\n");
 		}
 		if (features != 0) {
 			if (((AUDCTL[0] | AUDCTL[1]) & 1) != 0)
@@ -119,18 +128,21 @@ void scan_song(int song)
 			if (is_silence) {
 				silence_run++;
 				if (silence_run >= silence_player_calls) {
-					int seconds = player_calls_to_seconds(i - silence_run);
-					printf("TIME %02d:%02d\n", seconds / 60, seconds % 60);
+					int duration = player_calls_to_milliseconds(i - silence_run);
+					printf("TIME %02d:%02d.%02d\n", duration / 60000, duration / 1000 % 60, duration / 10 % 100);
 					return;
 				}
 			}
 			else
 				silence_run = 0;
 			if (i > loop_player_calls) {
-				if (memcmp(registers_dump, p - 18 * loop_player_calls, 18 * loop_player_calls) == 0) {
-					int seconds = player_calls_to_seconds(i - loop_player_calls);
-					printf("TIME %02d:%02d LOOP\n", seconds / 60, seconds % 60);
-					return;
+				unsigned char *q;
+				for (q = registers_dump; q < p - loop_bytes; q += 18) {
+					if (memcmp(q, p - loop_bytes, loop_bytes) == 0) {
+						int duration = player_calls_to_milliseconds(i - loop_player_calls);
+						printf("TIME %02d:%02d.%02d LOOP\n", duration / 60000, duration / 1000 % 60, duration / 10 % 100);
+						return;
+					}
 				}
 			}
 		}
@@ -144,11 +156,10 @@ int main(int argc, char *argv[])
 	const char *input_file;
 	int scan_seconds = 15 * 60;
 	int silence_seconds = 10;
-	int loop_seconds = 5 * 60;
+	int loop_seconds = 3 * 60;
 	FILE *fp;
 	static unsigned char module[ASAP_MODULE_MAX];
 	int module_len;
-	const ASAP_ModuleInfo *module_info;
 	int song;
 	if (argc != 3) {
 		print_help();
