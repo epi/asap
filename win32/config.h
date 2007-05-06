8c585fa7d2745392fa09093fceaeca118f5b27c2
Piotr Fusik <fox@scene.pl>
2007-05-06 15:16:15 +0000

    GSPlayer plugin for Windows CE

--- win32/config.h
+++ win32/config.h
@@ -1,5 +1,7 @@
-/* config.h for Win32 / x86 / Microsoft Compilers  */
+/* config.h for Win32 / Microsoft Compilers  */
 
 #define STEREO_SOUND 1
 
+#ifndef _WIN32_WCE
 #define WORDS_UNALIGNED_OK 1
+#endif
