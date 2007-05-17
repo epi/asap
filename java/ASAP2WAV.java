df1c5f334715686e95e18805ca4651e84155dff5
Piotr Fusik <fox@scene.pl>
2007-05-17 08:37:55 +0000

    implemented all options present in the C version

--- java/ASAP2WAV.java
+++ java/ASAP2WAV.java
@@ -29,6 +29,49 @@ public class ASAP2WAV
 {
 	private static final int BITS_PER_SAMPLE = 16;
 
+	private static boolean noInputFiles = true;
+	private static String outputFilename = null;
+	private static boolean outputHeader = true;
+	private static int song = -1;
+	private static boolean use16bit = true;
+	private static int duration = -1;
+
+	private static void printHelp()
+	{
+		System.out.print(
+			"Usage: java ASAP2WAV [OPTIONS] INPUTFILE...\n" +
+			"Each INPUTFILE must be in a supported format:\n" +
+			"SAP, CMC, CMR, DMC, MPT, MPD, RMT, TMC, TM8 or TM2.\n" +
+			"Options:\n" +
+			"-o FILE     --output=FILE      Set output file name\n" +
+			"-o -        --output=-         Write to standard output\n" +
+			"-s SONG     --song=SONG        Select subsong number (zero-based)\n" +
+			"-t TIME     --time=TIME        Set output length MM:SS\n" +
+			"-b          --byte-samples     Output 8-bit samples\n" +
+			"-w          --word-samples     Output 16-bit samples (default)\n" +
+			"            --raw              Output raw audio (no WAV header)\n" +
+			"-h          --help             Display this information\n" +
+			"-v          --version          Display version information\n"
+		);
+		noInputFiles = false;
+	}
+
+	private static void printVersion()
+	{
+		System.out.println("ASAP2WAV " + ASAP.VERSION);
+		noInputFiles = false;
+	}
+
+	private static void setSong(String s)
+	{
+		song = Integer.parseInt(s);
+	}
+
+	private static void setTime(String s)
+	{
+		duration = ASAP.parseDuration(s);
+	}
+
 	private static void writeTag(OutputStream os, String tag) throws IOException
 	{
 		os.write(tag.charAt(0));
@@ -51,44 +94,97 @@ public class ASAP2WAV
 		os.write(x >> 24);
 	}
 
-	public static void main(String[] args) throws IOException
+	private static void processFile(String inputFilename) throws IOException
 	{
-		if (args.length != 2) {
-			System.err.println("Usage: java ASAP2WAV inputfile outputfile");
-			System.exit(1);
-		}
-		InputStream is = new FileInputStream(args[0]);
+		InputStream is = new FileInputStream(inputFilename);
 		byte[] module = new byte[ASAP.MODULE_MAX];
 		int module_len = is.read(module);
 		is.close();
 		ASAP asap = new ASAP();
-		asap.load(args[0], module, module_len);
+		asap.load(inputFilename, module, module_len);
 		ASAP_ModuleInfo module_info = asap.getModuleInfo();
-		int song = module_info.default_song;
-		int duration = 10000;
+		if (song < 0)
+			song = module_info.default_song;
+		if (duration < 0) {
+			duration = module_info.durations[song];
+			if (duration < 0)
+				duration = 180 * 1000;
+		}
 		asap.playSong(song, duration);
-		int block_size = module_info.channels * (BITS_PER_SAMPLE / 8);
-		int bytes_per_second = ASAP.SAMPLE_RATE * block_size;
-		int n_bytes = duration * (ASAP.SAMPLE_RATE / 100) / 10 * block_size;
-		OutputStream os = new FileOutputStream(args[1]);
-		writeTag(os, "RIFF");
-		writeInt(os, n_bytes + 36);
-		writeTag(os, "WAVE");
-		writeTag(os, "fmt ");
-		writeInt(os, 16);
-		writeShort(os, 1);
-		writeShort(os, module_info.channels);
-		writeInt(os, ASAP.SAMPLE_RATE);
-		writeInt(os, bytes_per_second);
-		writeShort(os, block_size);
-		writeShort(os, BITS_PER_SAMPLE);
-		writeTag(os, "data");
-		writeInt(os, n_bytes);
+		if (outputFilename == null) {
+			int i = inputFilename.lastIndexOf('.');
+			outputFilename = inputFilename.substring(0, i) + ".wav";
+		}
+		OutputStream os;
+		if (outputFilename.equals("-"))
+			os = System.out;
+		else
+			os = new FileOutputStream(outputFilename);
+		int n_bytes;
+		if (outputHeader) {
+			int block_size = module_info.channels * (BITS_PER_SAMPLE / 8);
+			int bytes_per_second = ASAP.SAMPLE_RATE * block_size;
+			n_bytes = duration * (ASAP.SAMPLE_RATE / 100) / 10 * block_size;
+			writeTag(os, "RIFF");
+			writeInt(os, n_bytes + 36);
+			writeTag(os, "WAVE");
+			writeTag(os, "fmt ");
+			writeInt(os, 16);
+			writeShort(os, 1);
+			writeShort(os, module_info.channels);
+			writeInt(os, ASAP.SAMPLE_RATE);
+			writeInt(os, bytes_per_second);
+			writeShort(os, block_size);
+			writeShort(os, BITS_PER_SAMPLE);
+			writeTag(os, "data");
+			writeInt(os, n_bytes);
+		}
 		byte[] buffer = new byte[8192];
 		do {
 			n_bytes = asap.generate(buffer, BITS_PER_SAMPLE);
 			os.write(buffer, 0, n_bytes);
 		}  while (n_bytes == buffer.length);
 		os.close();
+		outputFilename = null;
+		song = -1;
+		duration = -1;
+		noInputFiles = false;
+	}
+
+	public static void main(String[] args) throws IOException
+	{
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			if (arg.charAt(0) != '-')
+				processFile(arg);
+			else if (arg.equals("-o"))
+				outputFilename = args[++i];
+			else if (arg.startsWith("--output="))
+				outputFilename = arg.substring(9);
+			else if (arg.equals("-s"))
+				setSong(args[++i]);
+			else if (arg.startsWith("--song="))
+				setSong(arg.substring(7));
+			else if (arg.equals("-t"))
+				setTime(args[++i]);
+			else if (arg.startsWith("--time="))
+				setTime(arg.substring(7));
+			else if (arg.equals("-b") || arg.equals("--byte-samples"))
+				use16bit = false;
+			else if (arg.equals("-w") || arg.equals("--word-samples"))
+				use16bit = true;
+			else if (arg.equals("--raw"))
+				outputHeader = false;
+			else if (arg.equals("-h") || arg.equals("--help"))
+				printHelp();
+			else if (arg.equals("-v") || arg.equals("--version"))
+				printVersion();
+			else
+				throw new IllegalArgumentException("unknown option: " + arg);
+		}
+		if (noInputFiles) {
+			printHelp();
+			System.exit(1);
+		}
 	}
 }
