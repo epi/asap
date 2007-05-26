de09d1e511cb4609162c533b604f6803bc9b748d
Piotr Fusik <fox@scene.pl>
2007-05-26 18:27:14 +0000

    finished midlet implementation

--- java/ASAPMIDlet.java
+++ java/ASAPMIDlet.java
@@ -21,82 +21,265 @@
  * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+import java.io.InputStream;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Vector;
 import javax.microedition.io.Connector;
 import javax.microedition.io.file.FileConnection;
 import javax.microedition.io.file.FileSystemRegistry;
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Gauge;
 import javax.microedition.lcdui.List;
+import javax.microedition.media.Manager;
+import javax.microedition.media.MediaException;
+import javax.microedition.media.Player;
+import javax.microedition.media.PlayerListener;
 import javax.microedition.midlet.MIDlet;
 
-public class ASAPMIDlet extends MIDlet implements CommandListener
+import net.sf.asap.ASAP;
+import net.sf.asap.ASAP_ModuleInfo;
+
+class FileList
 {
-	private List fileList;
-	private Command selectCommand;
-	private Command exitCommand;
+	String caption;
+	String path;
+	String[] names;
+	FileList parent;
+	FileList[] sublists;
 
-	private String currentPath = "";
-
-	private void reloadFileList()
+	private static boolean lessOrEqual(String name1, String name2)
 	{
-		for (int i = fileList.size(); --i >= 0; )
-			fileList.delete(i);
-		Enumeration e;
-		if (currentPath.length() == 0) {
-			fileList.setTitle("Select File");
-			e = FileSystemRegistry.listRoots();
-		}
-		else {
-			fileList.setTitle(currentPath);
-			fileList.append("..", null);
-			try {
-				FileConnection fc = (FileConnection) Connector.open("file:///" + currentPath, Connector.READ);
-				e = fc.list();
-			} catch (IOException ex) {
-				// TODO
-				return;
-			}
-		}
-		while (e.hasMoreElements())
-			fileList.append((String) e.nextElement(), null);
+		boolean dir1 = name1.endsWith("/");
+		boolean dir2 = name2.endsWith("/");
+		if (dir1 != dir2)
+			return dir1;
+		return (name1.compareTo(name2) <= 0);
 	}
+
+	private void sort(int start, int end)
+	{
+		while (start + 1 < end) {
+			int left = start + 1;
+			int right = end;
+			String pivot = names[start];
+			String tmp;
+			while (left < right) {
+				if (lessOrEqual(names[left], pivot))
+					left++;
+				else {
+					right--;
+					tmp = names[left];
+					names[left] = names[right];
+					names[right] = tmp;
+				}
+			}
+			left--;
+			tmp = names[left];
+			names[left] = names[start];
+			names[start] = tmp;
+			sort(start, left);
+			start = right;
+		}
+	}
+
+	FileList(String caption, String path, FileList parent, Enumeration contents)
+	{
+		this.caption = caption;
+		this.path = path;
+		this.parent = parent;
+		Vector v = new Vector();
+		int start = 0;
+		if (parent != null) {
+			v.addElement("..");
+			start = 1;
+		}
+		while (contents.hasMoreElements())
+			v.addElement(contents.nextElement());
+		int n = v.size();
+		names = new String[n];
+		sublists = new FileList[n];
+		for (int i = 0; i < n; i++)
+			names[i] = (String) v.elementAt(i);
+		sort(start, n);
+	}
+}
+
+class ASAPInputStream extends InputStream
+{
+	private ASAP asap;
+	private Gauge gauge;
+	private int[] header = new int[11];
+	private byte[] buffer = new byte[4096];
+	private int buffer_len;
+	private int offset = 0;
+	private int position = 0;
+
+	private static final int BITS_PER_SAMPLE = 8;
+
+	ASAPInputStream(ASAP asap, Gauge gauge)
+	{
+		this.asap = asap;
+		this.gauge = gauge;
+		ASAP_ModuleInfo module_info = asap.getModuleInfo();
+		int song = module_info.default_song;
+		int duration = module_info.durations[song];
+		if (duration < 0)
+			duration = 180000;
+		asap.playSong(song, duration);
+		int channels = module_info.channels;
+		int block_size = channels * (BITS_PER_SAMPLE / 8);
+		int bytes_per_second = ASAP.SAMPLE_RATE * block_size;
+		int n_bytes = duration * (ASAP.SAMPLE_RATE / 100) / 10 * block_size;
+		gauge.setMaxValue(n_bytes / buffer.length);
+		header[0] = 'R' + ('I' << 8) + ('F' << 16) + ('F' << 24);
+		header[1] = n_bytes + 36;
+		header[2] = 'W' + ('A' << 8) + ('V' << 16) + ('E' << 24);
+		header[3] = 'f' + ('m' << 8) + ('t' << 16) + (' ' << 24);
+		header[4] = 16;
+		header[5] = 1 + (channels << 16);
+		header[6] = ASAP.SAMPLE_RATE;
+		header[7] = bytes_per_second;
+		header[8] = block_size + (BITS_PER_SAMPLE << 16);
+		header[9] = 'd' + ('a' << 8) + ('t' << 16) + ('a' << 24);
+		header[10] = n_bytes;
+		buffer_len = asap.generate(buffer, BITS_PER_SAMPLE);
+	}
+
+	public int read()
+	{
+		int i = offset;
+		if (i < 44)
+			i = header[i >> 2] >> ((i & 3) << 3);
+		else {
+			i -= 44;
+			if (i >= buffer_len) {
+				if (buffer_len < buffer.length)
+					return -1;
+				buffer_len = asap.generate(buffer, BITS_PER_SAMPLE);
+				gauge.setValue(++position);
+				i = 0;
+				offset = 44;
+			}
+			i = buffer[i];
+		}
+		offset++;
+		return i & 0xff;
+	}
+}
+
+public class ASAPMIDlet extends MIDlet implements CommandListener, PlayerListener
+{
+	private Command selectCommand;
+	private Command stopCommand;
+	private Command exitCommand;
+	private FileList fileList;
+	private List listControl;
+	private byte[] module;
+	private ASAP asap;
+	private Player player;
 
 	public ASAPMIDlet()
 	{
 	}
 
+	private void displayFileList(FileList fileList)
+	{
+		this.fileList = fileList;
+		this.listControl = new List(fileList.caption, List.IMPLICIT, fileList.names, null);
+		listControl.addCommand(selectCommand);
+		listControl.addCommand(exitCommand);
+		listControl.setCommandListener(this);
+		Display.getDisplay(this).setCurrent(listControl);
+	}
+
+	private void displayError(String message)
+	{
+		Alert alert = new Alert("Error", message, null, AlertType.ERROR);
+		Display.getDisplay(this).setCurrent(alert);
+	}
+
 	public void startApp()
 	{
-		fileList = new List(null, List.IMPLICIT);
 		selectCommand = new Command("Select", Command.ITEM, 1);
-		fileList.addCommand(selectCommand);
-		exitCommand = new Command("Exit", Command.EXIT, 1);
-		fileList.addCommand(exitCommand);
-		fileList.setCommandListener(this);
-		reloadFileList();
-		Display.getDisplay(this).setCurrent(fileList);
+		stopCommand = new Command("Stop", Command.STOP, 1);
+		exitCommand = new Command("Exit", Command.EXIT, 2);
+		module = new byte[ASAP.MODULE_MAX];
+		asap = new ASAP();
+		displayFileList(new FileList("Select File", "", null, FileSystemRegistry.listRoots()));
 	}
 
 	public void commandAction(Command c, Displayable s)
 	{
 		if (c == selectCommand || c == List.SELECT_COMMAND) {
-			String filename = fileList.getString(fileList.getSelectedIndex());
+			int index = listControl.getSelectedIndex();
+			String filename = fileList.names[index];
 			if (filename.equals("..")) {
-				int i = currentPath.lastIndexOf('/', currentPath.length() - 2);
-				currentPath = currentPath.substring(0, i + 1);
+				displayFileList(fileList.parent);
+				return;
 			}
-			else {
-				currentPath += filename;
+			String path = fileList.path + filename;
+			try {
+				FileList newList = fileList.sublists[index];
+				if (newList != null) {
+					displayFileList(newList);
+					return;
+				}
+				FileConnection fc = (FileConnection) Connector.open("file:///" + path, Connector.READ);
+				if (fc.isDirectory()) {
+					newList = new FileList(filename, path, fileList, fc.list());
+					fileList.sublists[index] = newList;
+					displayFileList(newList);
+					return;
+				}
+				InputStream is = fc.openInputStream();
+				int module_len = 0;
+				for (;;) {
+					int i = is.read(module, module_len, ASAP.MODULE_MAX - module_len);
+					if (i <= 0)
+						break;
+					module_len += i;
+				}
+				is.close();
+				asap.load(filename, module, module_len);
+				Gauge gauge = new Gauge(filename, false, 1, 0);
+				is = new ASAPInputStream(asap, gauge);
+				player = Manager.createPlayer(is, "audio/x-wav");
+				player.addPlayerListener(this);
+				player.start();
+				Form playForm = new Form("ASAP");
+				playForm.append(gauge);
+				playForm.addCommand(stopCommand);
+				playForm.addCommand(exitCommand);
+				playForm.setCommandListener(this);
+				Display.getDisplay(this).setCurrent(playForm);
+			} catch (Exception ex) {
+				displayError(ex.toString());
 			}
-			reloadFileList();
+		}
+		else if (c == stopCommand) {
+			try {
+				player.stop();
+			} catch (MediaException ex) {
+			}
+			Display.getDisplay(this).setCurrent(listControl);
 		}
 		else if (c == exitCommand)
 			notifyDestroyed();
+	}
+
+	public void playerUpdate(Player player, String event, Object eventData)
+	{
+		if (event == PlayerListener.END_OF_MEDIA)
+			Display.getDisplay(this).setCurrent(listControl);
+		else if (event == PlayerListener.ERROR)
+			displayError((String) eventData);
 	}
 
 	public void pauseApp()
--- java/mf2jad.pl
+++ java/mf2jad.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 use strict;
 my ($name) = @ARGV or die "Usage: mf2jad APPNAME\n";
-open MF, "MIDLET_MANIFEST.MF" or die "MIDLET_MANIFEST.MF: $!\n";
+open MF, "$name.MF" or die "$name.MF: $!\n";
 open JAD, ">$name.jad" or die "$name.jad: $!\n";
 print JAD "MIDlet-Jar-URL: $name.jar\n";
 print JAD "MIDlet-Jar-Size: ", -s "$name.jar", "\n";
