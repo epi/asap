9b1bc22aa1ae90436fe4736ced099fa6bf4f9596
Piotr Fusik <fox@scene.pl>
2007-07-05 11:37:50 +0000

    header comment

--- asap_internal.h
+++ asap_internal.h
@@ -1,3 +1,26 @@
+/*
+ * asap_internal.h - private interface of the ASAP engine
+ *
+ * Copyright (C) 2005-2007  Piotr Fusik
+ *
+ * This file is part of ASAP (Another Slight Atari Player),
+ * see http://asap.sourceforge.net
+ *
+ * ASAP is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * ASAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ASAP; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #ifndef _ASAP_INTERNAL_H_
 #define _ASAP_INTERNAL_H_
 
