307842ed6c7edef2b7fd86e12d6f175177f40f39
Piotr Fusik <fox@scene.pl>
2009-06-01 08:08:14 +0000

    made shorter

--- raw2c.pl
+++ raw2c.pl
@@ -6,11 +6,9 @@ for (@ARGV) {
 	s!.*[/\\]!!;
 	y/0-9A-Za-z/_/c;
 	print "static const unsigned char ${_}[] = {\n\t";
-	my $buf;
-	print join ', ', map sprintf('0x%02X', $_), unpack 'C*', $buf
-		if read INPUT, $buf, 16;
-	print ",\n\t", join ', ', map sprintf('0x%02X', $_), unpack 'C*', $buf
-		while read INPUT, $buf, 16;
-	close INPUT;
+	$/ = \1;
+	$. = 0;
+	printf '%s0x%02X', $. % 16 != 1 ? ', ' : $. > 1 ? ",\n\t" : '', ord
+		while <INPUT>;
 	print "\n};\n"
 }
