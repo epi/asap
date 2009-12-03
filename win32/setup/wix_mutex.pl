b80a00c18c47a04782cf68cb27b787468c451d21
Piotr Fusik <fox@scene.pl>
2009-12-03 10:29:13 +0000

    XBMC doesn't support file associations

--- win32/setup/wix_mutex.pl
+++ win32/setup/wix_mutex.pl
@@ -1,5 +1,5 @@
 # Generate mutual exclusion conditions
-@c = qw(wasap.ext asap_dsf.ext in_asap.ext foo_asap.ext xbmc_asap.ext ASAP_Apollo.ext);
+@c = qw(wasap.ext asap_dsf.ext in_asap.ext foo_asap.ext ASAP_Apollo.ext);
 @a = map "\$$_=3 OR (\$$_=-1 AND ?$_=3)", @c; # action is install or (action is nothing and already installed)
 for $a (@a[0 .. $#a - 1]) {
 	@b = @a[++$i .. $#a];
