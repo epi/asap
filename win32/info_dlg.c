a88125ae543d9a90f577005ec2cc5e4bad55c407
Piotr Fusik <fox@scene.pl>
2010-01-15 09:51:52 +0000

    split gui.c into info_dlg.c and settings_dlg.c

--- win32/info_dlg.c
+++ win32/info_dlg.c
@@ -1,7 +1,7 @@
 /*
- * gui.c - settings and file information dialog boxes
+ * info_dlg.c - file information dialog box
  *
- * Copyright (C) 2007-2009  Piotr Fusik
+ * Copyright (C) 2007-2010  Piotr Fusik
  *
  * This file is part of ASAP (Another Slight Atari Player),
  * see http://asap.sourceforge.net
@@ -24,184 +24,9 @@
 #include <windows.h>
 #include <stdio.h>
 #include <string.h>
-#include <tchar.h>
 
 #include "asap.h"
 #include "gui.h"
-
-#ifndef WASAP
-
-int song_length = -1;
-int silence_seconds = -1;
-BOOL play_loops = FALSE;
-int mute_mask = 0;
-static int saved_mute_mask;
-#ifdef WINAMP
-BOOL playing_info = FALSE;
-#endif
-
-static void enableTimeInput(HWND hDlg, BOOL enable)
-{
-	EnableWindow(GetDlgItem(hDlg, IDC_MINUTES), enable);
-	EnableWindow(GetDlgItem(hDlg, IDC_SECONDS), enable);
-}
-
-static void setFocusAndSelect(HWND hDlg, int nID)
-{
-	HWND hWnd = GetDlgItem(hDlg, nID);
-	SetFocus(hWnd);
-	SendMessage(hWnd, EM_SETSEL, 0, -1);
-}
-
-static BOOL getDlgInt(HWND hDlg, int nID, int *result)
-{
-	BOOL translated;
-	UINT r = GetDlgItemInt(hDlg, nID, &translated, FALSE);
-	if (!translated) {
-		MessageBox(hDlg, _T("Invalid number"), _T("Error"), MB_OK | MB_ICONERROR);
-		return FALSE;
-	}
-	*result = (int) r;
-	return TRUE;
-}
-
-static INT_PTR CALLBACK settingsDialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	int i;
-	switch (uMsg) {
-	case WM_INITDIALOG:
-		if (song_length <= 0) {
-			CheckRadioButton(hDlg, IDC_UNLIMITED, IDC_LIMITED, IDC_UNLIMITED);
-			SetDlgItemInt(hDlg, IDC_MINUTES, DEFAULT_SONG_LENGTH / 60, FALSE);
-			SetDlgItemInt(hDlg, IDC_SECONDS, DEFAULT_SONG_LENGTH % 60, FALSE);
-			enableTimeInput(hDlg, FALSE);
-		}
-		else {
-			CheckRadioButton(hDlg, IDC_UNLIMITED, IDC_LIMITED, IDC_LIMITED);
-			SetDlgItemInt(hDlg, IDC_MINUTES, (UINT) song_length / 60, FALSE);
-			SetDlgItemInt(hDlg, IDC_SECONDS, (UINT) song_length % 60, FALSE);
-			enableTimeInput(hDlg, TRUE);
-		}
-		if (silence_seconds <= 0) {
-			CheckDlgButton(hDlg, IDC_SILENCE, BST_UNCHECKED);
-			SetDlgItemInt(hDlg, IDC_SILSECONDS, DEFAULT_SILENCE_SECONDS, FALSE);
-			EnableWindow(GetDlgItem(hDlg, IDC_SILSECONDS), FALSE);
-		}
-		else {
-			CheckDlgButton(hDlg, IDC_SILENCE, BST_CHECKED);
-			SetDlgItemInt(hDlg, IDC_SILSECONDS, (UINT) silence_seconds, FALSE);
-			EnableWindow(GetDlgItem(hDlg, IDC_SILSECONDS), TRUE);
-		}
-		CheckRadioButton(hDlg, IDC_LOOPS, IDC_NOLOOPS, play_loops ? IDC_LOOPS : IDC_NOLOOPS);
-		saved_mute_mask = mute_mask;
-		for (i = 0; i < 8; i++)
-			CheckDlgButton(hDlg, IDC_MUTE1 + i, ((mute_mask >> i) & 1) != 0 ? BST_CHECKED : BST_UNCHECKED);
-		return TRUE;
-	case WM_COMMAND:
-		if (HIWORD(wParam) == BN_CLICKED) {
-			WORD wCtrl = LOWORD(wParam);
-			BOOL enabled;
-			switch (wCtrl) {
-			case IDC_UNLIMITED:
-			case IDC_LIMITED:
-				enabled = (wCtrl == IDC_LIMITED);
-				enableTimeInput(hDlg, enabled);
-				if (enabled)
-					setFocusAndSelect(hDlg, IDC_MINUTES);
-				return TRUE;
-			case IDC_SILENCE:
-				enabled = (IsDlgButtonChecked(hDlg, IDC_SILENCE) == BST_CHECKED);
-				EnableWindow(GetDlgItem(hDlg, IDC_SILSECONDS), enabled);
-				if (enabled)
-					setFocusAndSelect(hDlg, IDC_SILSECONDS);
-				return TRUE;
-			case IDC_LOOPS:
-			case IDC_NOLOOPS:
-				return TRUE;
-			case IDC_MUTE1:
-			case IDC_MUTE1 + 1:
-			case IDC_MUTE1 + 2:
-			case IDC_MUTE1 + 3:
-			case IDC_MUTE1 + 4:
-			case IDC_MUTE1 + 5:
-			case IDC_MUTE1 + 6:
-			case IDC_MUTE1 + 7:
-				i = 1 << (wCtrl - IDC_MUTE1);
-				if (IsDlgButtonChecked(hDlg, wCtrl) == BST_CHECKED)
-					mute_mask |= i;
-				else
-					mute_mask &= ~i;
-				ASAP_MutePokeyChannels(&asap, mute_mask);
-				return TRUE;
-			case IDOK:
-			{
-				int new_song_length;
-				if (IsDlgButtonChecked(hDlg, IDC_UNLIMITED) == BST_CHECKED)
-					new_song_length = -1;
-				else {
-					int minutes;
-					int seconds;
-					if (!getDlgInt(hDlg, IDC_MINUTES, &minutes)
-					 || !getDlgInt(hDlg, IDC_SECONDS, &seconds))
-						return TRUE;
-					new_song_length = 60 * minutes + seconds;
-				}
-				if (IsDlgButtonChecked(hDlg, IDC_SILENCE) != BST_CHECKED)
-					silence_seconds = -1;
-				else if (!getDlgInt(hDlg, IDC_SILSECONDS, &silence_seconds))
-					return TRUE;
-				song_length = new_song_length;
-				play_loops = (IsDlgButtonChecked(hDlg, IDC_LOOPS) == BST_CHECKED);
-			}
-				EndDialog(hDlg, IDOK);
-				return TRUE;
-			case IDCANCEL:
-				mute_mask = saved_mute_mask;
-				ASAP_MutePokeyChannels(&asap, mute_mask);
-				EndDialog(hDlg, IDCANCEL);
-				return TRUE;
-			}
-		}
-		break;
-	default:
-		break;
-	}
-	return FALSE;
-}
-
-BOOL settingsDialog(HINSTANCE hInstance, HWND hwndParent)
-{
-	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_SETTINGS), hwndParent, settingsDialogProc) == IDOK;
-}
-
-int getSongDuration(const ASAP_ModuleInfo *module_info, int song)
-{
-	int duration = module_info->durations[song];
-	if (duration < 0)
-		return 1000 * song_length;
-	if (play_loops && module_info->loops[song])
-		return 1000 * song_length;
-	return duration;
-}
-
-int playSong(int song)
-{
-	int duration = asap.module_info.durations[song];
-	if (duration < 0) {
-		if (silence_seconds > 0)
-			ASAP_DetectSilence(&asap, silence_seconds);
-		duration = 1000 * song_length;
-	}
-	if (play_loops && asap.module_info.loops[song])
-		duration = 1000 * song_length;
-	ASAP_PlaySong(&asap, song, duration);
-	ASAP_MutePokeyChannels(&asap, mute_mask);
-	return duration;
-}
-
-#endif /* WASAP */
-
-#if defined(WASAP) || defined(WINAMP) || defined(APOLLO)
 
 HWND infoDialog = NULL;
 static byte saved_module[ASAP_MODULE_MAX];
@@ -568,5 +393,3 @@ void updateInfoDialog(const char *filename, int song)
 		EnableWindow(GetDlgItem(infoDialog, IDC_CONVERT), FALSE);
 	}
 }
-
-#endif
