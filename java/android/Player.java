937626b0d4fc1ed63690ad5f26467eb80f6913e9
Piotr Fusik <fox@scene.pl>
2010-10-13 17:28:59 +0200

    [android] Check for minimum audio buffer size (8k was too small for stereo).

--- java/android/Player.java
+++ java/android/Player.java
@@ -69,12 +69,14 @@ public class Player extends Activity implements Runnable, OnClickListener
 		int duration = module_info.loops[song] ? -1 : module_info.durations[song];
 		asap.playSong(song, duration);
 
-		final byte[] buffer = new byte[8192];
+		int config = module_info.channels == 1 ? AudioFormat.CHANNEL_CONFIGURATION_MONO : AudioFormat.CHANNEL_CONFIGURATION_STEREO;
+		int len = AudioTrack.getMinBufferSize(ASAP.SAMPLE_RATE, config, AudioFormat.ENCODING_PCM_8BIT);
+		if (len < 16384)
+			len = 16384;
+		final byte[] buffer = new byte[len];
 		AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, ASAP.SAMPLE_RATE,
-			module_info.channels == 1 ? AudioFormat.CHANNEL_CONFIGURATION_MONO : AudioFormat.CHANNEL_CONFIGURATION_STEREO,
-			AudioFormat.ENCODING_PCM_8BIT, buffer.length, AudioTrack.MODE_STREAM);
+			config, AudioFormat.ENCODING_PCM_8BIT, len, AudioTrack.MODE_STREAM);
 		audioTrack.play();
-		int len;
 		do {
 			synchronized (this) {
 				if (this.stop) {
