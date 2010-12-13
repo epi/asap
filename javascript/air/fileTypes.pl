cddb976db2039e61eb515a402c561e9c1478257b
Piotr Fusik <fox@scene.pl>
2010-12-13 11:19:01 +0100

    [air] Associate file types.

---
+++ javascript/air/fileTypes.pl
@@ -0,0 +1,9 @@
+for (qw(sap cmc cm3 cmr cms dmc dlt mpt mpd rmt tmc tm8 tm2)) {
+	print qq{		<fileType>
+			<name>$_.file</name>
+			<extension>$_</extension>
+			<description>8-bit Atari music</description>
+			<contentType>application/x-$_</contentType>
+		</fileType>
+};
+}
