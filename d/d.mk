ccc5ab72ac81595c5aae6897385003d26167f005
Adrian Matoga <epi@atari8.info>
2011-07-17 16:40:13 +0200

    [d] Added common target for all D ports. Removed dir separator substitution. Added alsa/pcm.d to built files.

--- d/d.mk
+++ d/d.mk
@@ -1,4 +1,5 @@
-DC = $(DO)dmd -release -inline -O -of$(subst /,\\,$@) $(subst /,\\,$^)
+DC = $(DO)dmd -release -inline -O -of$@ $^
+#$(subst /,\\,$@) $(subst /,\\,$^)
 
 # no user-configurable paths below this line
 
@@ -6,11 +7,14 @@ ifndef DO
 $(error Use "Makefile" instead of "d.mk")
 endif
 
+d: d/asap2wav.exe d/asapplay.exe
+.PHONY: d
+
 d/asap2wav.exe: $(srcdir)d/asap2wav.d d/asap.d
 	$(DC)
 CLEAN += d/asap2wav.exe d/asap2wav.obj asap2wav.map
 
-d/asapplay.exe: $(call src,d/asapplay.d d/waveout.d) d/asap.d
+d/asapplay.exe: $(call src,d/asapplay.d d/waveout.d d/alsa/pcm.d) d/asap.d
 	$(DC)
 CLEAN += d/asapplay.exe d/asapplay.obj asapplay.map
 
