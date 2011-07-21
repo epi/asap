c95829573c217ab598030fa06c66dbed9068fab5
Adrian Matoga <epi@atari8.info>
2011-07-21 20:01:42 +0200

    [d] Build on Linux.

--- d/d.mk
+++ d/d.mk
@@ -1,4 +1,4 @@
-DC = $(DO)dmd -release -inline -O -of$(subst /,\\,$@) $(subst /,\\,$^)
+DC = $(DO)dmd -release -inline -O $(if $(WINDIR),-of$(@:/=\\) $(^:/=\\),-of$@ $^)
 
 # no user-configurable paths below this line
 
@@ -13,7 +13,7 @@ d/asap2wav.exe: $(srcdir)d/asap2wav.d d/asap.d
 	$(DC)
 CLEAN += d/asap2wav.exe d/asap2wav.obj asap2wav.map
 
-d/asapplay.exe: $(call src,d/asapplay.d d/waveout.d) d/asap.d
+d/asapplay.exe: $(call src,d/asapplay.d d/waveout.d d/alsa/pcm.d) d/asap.d
 	$(DC)
 CLEAN += d/asapplay.exe d/asapplay.obj asapplay.map
 
