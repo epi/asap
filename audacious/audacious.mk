2bfb370785a3585f5e7e4b35d7d5d383a4fd5efe
Piotr Fusik <fox@scene.pl>
2013-07-08 13:24:28 +0200

    [audacious] Recompile plugin on Audacious upgrade.

--- audacious/audacious.mk
+++ audacious/audacious.mk
@@ -1,3 +1,4 @@
+AUDACIOUS_VERSION := $(shell pkg-config --modversion audacious 2>/dev/null)
 AUDACIOUS_CFLAGS = `pkg-config --cflags gtk+-3.0`
 AUDACIOUS_INPUT_PLUGIN_DIR = `pkg-config --variable=input_plugin_dir audacious`
 
@@ -7,15 +8,15 @@ ifndef DO
 $(error Use "Makefile" instead of "audacious.mk")
 endif
 
-asap-audacious: asapplug.so
+asap-audacious: asapplug-$(AUDACIOUS_VERSION).so
 .PHONY: asap-audacious
 
-asapplug.so: $(call src,audacious/asapplug.c asap.[ch])
+asapplug-$(AUDACIOUS_VERSION).so: $(call src,audacious/asapplug.c asap.[ch])
 	$(CC) $(AUDACIOUS_CFLAGS)
-CLEAN += asapplug.so
+CLEAN += asapplug-*.so
 
-install-audacious: asapplug.so
-	$(call INSTALL_PROGRAM,asapplug.so,$(AUDACIOUS_INPUT_PLUGIN_DIR))
+install-audacious: asapplug-$(AUDACIOUS_VERSION).so
+	$(call INSTALL_PROGRAM,asapplug-$(AUDACIOUS_VERSION).so,$(AUDACIOUS_INPUT_PLUGIN_DIR),asapplug.so)
 .PHONY: install-audacious
 
 uninstall-audacious:
