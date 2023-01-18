fc31c696f1dbb98926eae2f936d58cb24bbe72fc
Piotr Fusik <fox@scene.pl>
2023-01-18 08:40:06 +0100

    [doc] Bump the copyright year.

--- win32/rmt/apokeysnd_dll.c
+++ win32/rmt/apokeysnd_dll.c
@@ -1,7 +1,7 @@
 /*
  * apokeysnd_dll.c - POKEY sound emulator for Raster Music Tracker
  *
- * Copyright (C) 2008-2022  Piotr Fusik
+ * Copyright (C) 2008-2023  Piotr Fusik
  *
  * This file is part of ASAP (Another Slight Atari Player),
  * see http://asap.sourceforge.net
@@ -55,6 +55,6 @@ __declspec(dllexport) int APokeySound_Generate(int cycles, unsigned char *buffer
 __declspec(dllexport) void APokeySound_About(const char **name, const char **author, const char **description)
 {
 	*name = "Another POKEY Sound Emulator, v5.2.0";
-	*author = "Piotr Fusik, (C) 2007-2022";
+	*author = "Piotr Fusik, (C) 2007-2023";
 	*description = "Part of ASAP, http://asap.sourceforge.net";
 }
